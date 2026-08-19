501a61fa076fd904af64d91c527d251fd94dedba
anxwball <aaron.nxwball@gmail.com>
2026-08-19 00:02:53 -0500

    feat: agregar modelo de tarea de la agenda

---
+++ agenda/models.py
@@ -0,0 +1,14 @@
+# agenda/models.py
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+
+from agenda.database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    status = Column(String, index=True)
+    due_date = Column(DateTime, index=True)
