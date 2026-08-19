0cffcbd0854b8aeb3f006892e51237a756697e7a
anxwball <aaron.nxwball@gmail.com>
2026-08-18 22:47:05 -0500

    feat: creacion de instancia de base de datos

---
+++ agenda/database.py
@@ -0,0 +1,18 @@
+# agenda/database.py
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./agenda.db")
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def init_db():
+    """Initialize the database by creating all tables."""
+    Base.metadata.create_all(bind=engine)
